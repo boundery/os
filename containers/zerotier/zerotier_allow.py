fde47c64d92888b61e144ac5fac71e0d4bc472d6
Ellen Wang <boundery.me@ellenwang.com>
2019-01-31 17:26:25 -0800

    Put private zerotier network on the built-in controller.
    This will ultimately make it easier to bring up the private
    network, since everything is now under our control.
    
    For now, the way to add hosts to the private network is
    to put their zerotier host ids in zerotier.conf like this:
        ZEROTIER_AUTHORIZED_HOSTS="<host-id>..."
    Then run "zerotier join <network-id>" on each host after
    the device boots and prints the network id on the console.

--- containers/zerotier/zerotier_allow.py
+++ containers/zerotier/zerotier_allow.py
@@ -5,27 +5,44 @@ import json
 import os
 import sys
 import time
-import subprocess
 
 if len(sys.argv) < 1:
     print("Usage: zerotier_allow.py <network name>")
     exit(-1)
 netname = sys.argv[1]
 
+# Get the "secret" authtoken for API authentication
+authtoken = None
+while True:
+    try:
+        with open("/var/lib/zerotier-one/authtoken.secret") as f:
+            # XXX is there a chance of a race with zerotier startup?
+            authtoken = f.read().strip()
+    except FileNotFoundError:
+        pass
+    if authtoken:
+        break
+    print("Waiting for zerotier authtoken to be created")
+    time.sleep(1)
+
 def zt_do(path, data = None):
     headers = { 'Connection': 'close',
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer %s' % os.getenv("ZEROTIER_TOKEN") }
+                'X-ZT1-Auth': authtoken }
 
-    url = "https://my.zerotier.com/api/%s" % path
+    url = "http://localhost:9993/%s" % path
 
-    if data:
-        r = requests.post(url, headers = headers, data = json.dumps(data))
-    else:
-        r = requests.get(url, headers = headers)
+    try:
+        if data:
+            r = requests.post(url, headers = headers, data = json.dumps(data))
+        else:
+            r = requests.get(url, headers = headers)
+    except requests.exceptions.ConnectionError:
+	# Fail soft if daemon isn't running (yet)
+        return None
 
     if r.status_code == 200 and len(r.text) != 0:
-        print("'%s'" % r.text)
+        #print("'%s'" % r.text)
         return r.json()
     else:
         return None
@@ -35,67 +52,79 @@ def zt_get(path):
 def zt_post(path, data):
     return zt_do(path, data)
 
+#Wait for the daemon, and get our hostid.
+hostid = None
+while True:
+    status = zt_get("status")
+    if status and status["online"]:
+        hostid = status["address"]
+        break
+    print("Waiting for zerotier-one to start")
+    time.sleep(5)
+print("Got host id: %s" % hostid)
+
 netid = None
 
-#Check to see if it is already there.
-networks = zt_get("network")
-for net in networks:
-    if net["config"]["name"] == netname:
+#Check to see if network is already there.
+networks = zt_get("controller/network")
+for id in networks:
+    net = zt_get("controller/network/" + id)
+    if net["name"] == netname:
         netid = net["id"]
+        print("Got network id: %s" % netid)
         break
 
 #XXX Would be cool to use 100.64.0.0/10 subnets here, but that is broken in ZT:
 #    https://github.com/zerotier/ZeroTierOne/issues/675
 #    If we do that, linux/windows/mac need to "allowGlobal" too.
 if not netid: #Create the network
-    print("Network %s doesn't exist, creating." % netname)
-    newnet = { 'description': 'Private network for internal services',
-               'config': { 'name': netname, 'multicastLimit': 16, 'mtu': 2800,
-                           'v6AssignMode': {'rfc4193': False, 'zt': False, '6plane': True},
-                           'v4AssignMode': {'zt': True},
-                           'tags': [], 'private': True,
-                           'ipAssignmentPools': [{'ipRangeEnd': '172.18.3.254', 'ipRangeStart': '172.18.2.1'}],
-                           'enableBroadcast': True,
-                           'routes': [{'via': None, 'target': '172.18.0.0/22'}]
-                         }
-             }
-    net = zt_post("network", newnet)
+    print("Network %s doesn't exist, creating" % netname)
+    newnet = {
+        'name': netname,
+        'multicastLimit': 16,
+        'mtu': 2800,
+        'v6AssignMode': {'rfc4193': False, 'zt': False, '6plane': True},
+        'v4AssignMode': {'zt': True},
+        'tags': [],
+        'private': True,
+        'ipAssignmentPools': [{
+            'ipRangeEnd': '172.18.3.254',
+            'ipRangeStart': '172.18.2.1'
+        }],
+        'enableBroadcast': True,
+        'routes': [{'via': None, 'target': '172.18.0.0/22'}],
+    }
+    net = zt_post("controller/network/%s______" % hostid, newnet)
     netid = net["id"]
-    print("Created network id is %s" % netid)
+    print("Created network id: %s" % netid)
 
-#Wait for the daemon, and get our hostid.
-hostid = None
-while not hostid:
-    status = subprocess.getoutput("/usr/sbin/zerotier-cli status")
-    if status.startswith("200 "):
-        hostid = status.split()[2]
-        print("Got hostid: %s" % hostid)
-    time.sleep(1)
+    # (Pre)authorize additional hosts, for devel bring-up only
+    for ahost in os.getenv("ZEROTIER_AUTHORIZED_HOSTS", "").split():
+        print("Authorizing host %s" % ahost)
+        zt_post("controller/network/%s/member/%s" % (netid, ahost),
+                {'authorized': True, 'activeBridge': True})
 
 #Join the network.  This is harmless if we're already a member.
 newjoin = os.system("/usr/sbin/zerotier-cli join %s" % netid) == 0
 
 #Poll until our request to join shows up.
 member = None
-pollcount = 0
-while not member:
-    member = zt_get("network/%s/member/%s" % (netid, hostid))
-    time.sleep(5)
-    pollcount += 1
-    if not member and pollcount > 10:
-        raise Exception("Timed out waiting for member to show up")
+while True:
+    member = zt_get("controller/network/%s/member/%s" % (netid, hostid))
+    if member:
+        break
+    print("Waiting to join network")
+    time.sleep(1)
 
 #Now authorize ourselves, if we're not already.
-if member["config"]["authorized"] != True:
-    print("Not authorized, authorizing with ZT Central")
-    newmember = {}
-    newmember["config"] = {}
-    newmember["config"]["authorized"] = True
-    newmember["config"]["activeBridge"] = True
-    newmember["name"] = "services"
-    #ZT Central doesn't autoset IPv4 addr because of "activeBridge", so set it here.
-    newmember["config"]["ipAssignments"] = [ "172.18.0.1" ]
-    zt_post("network/%s/member/%s" % (netid, hostid),
+if member["authorized"] != True:
+    print("Authorizing")
+    newmember = {
+        'authorized': True,
+        'activeBridge': True,
+        'ipAssignments': [ '172.18.0.1' ],
+    }
+    zt_post("controller/network/%s/member/%s" % (netid, hostid),
             newmember)
 
 #XXX Poll here checking if our local daemon is authorized and connected, if
