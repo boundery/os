7bd201557cdda2bcfbea496495ce714792c72aaf
Ellen Wang <boundery.me@ellenwang.com>
2019-07-22 14:35:51 -0700

    Move zerotier interface and address discovery into the container
    Instead of exposing the details in init (and having to do it
    in sh), it's much better to do all the work in zerotier_allow.py.
    Now it can create the networks, join them, wait for the interfaces
    to appear, and return only the necessary information back to init.

--- containers/zerotier/zerotier_allow.py
+++ containers/zerotier/zerotier_allow.py
@@ -1,10 +1,15 @@
 import os
 import sys
 import time
+import re
 from zerotier import zt_get, zt_post
 
+def stderr(msg):
+    print(msg, file = sys.stderr)
+    sys.stderr.flush()
+
 if len(sys.argv) < 1:
-    print("Usage: zerotier_allow.py <network name>")
+    stderr("Usage: zerotier_allow.py <network name>")
     exit(-1)
 netname = sys.argv[1]
 
@@ -15,14 +20,15 @@ while True:
     if status and status["online"]:
         hostid = status["address"]
         break
-    print("Waiting for zerotier-one to start")
+    stderr("Waiting for zerotier-one to start")
     time.sleep(5)
-print("Got host id: %s" % hostid)
+stderr("Got host id: %s" % hostid)
 
 netid = None
+adhocid = "ff05390539000000"
 
 #Join the adhoc network.
-zt_post("network/ff05390539000000", {})
+zt_post("network/%s" % adhocid, {})
 
 #Check to see if network is already there.
 networks = zt_get("controller/network")
@@ -30,14 +36,14 @@ for id in networks:
     net = zt_get("controller/network/" + id)
     if net["name"] == netname:
         netid = net["id"]
-        print("Got network id: %s" % netid)
+        stderr("Got network id: %s" % netid)
         break
 
 #XXX Would be cool to use 100.64.0.0/10 subnets here, but that is broken in ZT:
 #    https://github.com/zerotier/ZeroTierOne/issues/675
 #    If we do that, linux/windows/mac need to "allowGlobal" too.
 if not netid: #Create the network
-    print("Network %s doesn't exist, creating" % netname)
+    stderr("Network %s doesn't exist, creating" % netname)
     newnet = {
         'name': netname,
         'multicastLimit': 16,
@@ -55,13 +61,7 @@ if not netid: #Create the network
     }
     net = zt_post("controller/network/%s______" % hostid, newnet)
     netid = net["id"]
-    print("Created network id: %s" % netid)
-
-    # (Pre)authorize additional hosts, for devel bring-up only
-    for ahost in os.getenv("ZEROTIER_AUTHORIZED_HOSTS", "").split():
-        print("Authorizing host %s" % ahost)
-        zt_post("controller/network/%s/member/%s" % (netid, ahost),
-                {'authorized': True, 'activeBridge': False})
+    stderr("Created network id: %s" % netid)
 
 #Join the network.  This is harmless if we're already a member.
 zt_post("network/%s" % netid, {})
@@ -72,12 +72,12 @@ while True:
     member = zt_get("controller/network/%s/member/%s" % (netid, hostid))
     if member:
         break
-    print("Waiting to join network")
+    stderr("Waiting to join network")
     time.sleep(1)
 
 #Now authorize ourselves, if we're not already.
 if member["authorized"] != True:
-    print("Authorizing")
+    stderr("Authorizing")
     newmember = {
         'authorized': True,
         'activeBridge': True,
@@ -85,3 +85,35 @@ if member["authorized"] != True:
     }
     zt_post("controller/network/%s/member/%s" % (netid, hostid),
             newmember)
+
+#Wait for interfaces to come up
+ztif = None
+bootstrap_ipv6 = None
+while True:
+    net = zt_get("network/%s" % netid)
+    if net["status"] == "OK":
+        ztif = net["portDeviceName"]
+        break
+    stderr("Waiting for %s interface to appear" % netname)
+    time.sleep(1)
+while True:
+    net = zt_get("network/%s" % adhocid)
+    if net["status"] == "OK":
+        m = None
+        for addr in net["assignedAddresses"]:
+            m = re.fullmatch("(fc[0-9a-f:]+)/40", addr)
+            if m:
+                break
+        if m:
+            bootstrap_ipv6 = m.group(1)
+            break
+        # bad bad bad
+        stderr("Bootstrap interface %s exists " +
+               "but does not have a suitable address." % adhocid)
+        stderr("assignedAddresses: %s" % net["assignedAddresses"])
+    stderr("Waiting for bootstrap interface to appear")
+    time.sleep(1)
+
+stderr("Private interface: %s" % ztif)
+stderr("Bootstrop address: %s" % bootstrap_ipv6)
+print("%s %s" % (ztif, bootstrap_ipv6))
